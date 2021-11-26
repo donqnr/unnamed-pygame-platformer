e858ee015168e97aed3dbc23498438c2c5da83b5
donqnr <84308115+donqnr@users.noreply.github.com>
2021-11-26 12:38:52 +0200

    Delete vars.py
    Deleting duplicate files

--- vars.py
+++
@@ -1,9 +0,0 @@
-from pygame import sprite
-
-active_sprites = sprite.Group() # Sprites that are updated each frame (Player, enemies)
-visible_sprites = sprite.Group() # Group to handle the visibility of a sprite
-enemy_sprites = sprite.Group() # Group for handling hit detection of an enemy
-player_sprites = sprite.Group() # Group for handling hit of a player
-bg_sprites = sprite.Group() # Group for background blocks, to be blitted before other elements of the level
-
-paused = False # Boolean to handle pausing
